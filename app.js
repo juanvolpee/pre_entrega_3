cc1eb3dfa19a9daa70e30bf9d62cac399edec563
juanvolpee <138537881+juanvolpee@users.noreply.github.com>
2024-02-05 20:33:20 -0300

    Create app.js

---
+++ app.js
@@ -0,0 +1,60 @@
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const ProductManager = require('./productManager');
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const productManagerInstance = new ProductManager('./productManager.js');
+
+app.get('/products', (req, res) => {
+  const { limit } = req.query;
+
+  let productList = productManagerInstance.getProducts();
+
+  if (limit) {
+    const limitNumber = parseInt(limit, 10);
+    if (!isNaN(limitNumber) && limitNumber > 0) {
+      productList = productList.slice(0, limitNumber);
+    } else {
+      return res.status(400).json({ error: 'El parámetro limit debe ser un número positivo.' });
+    }
+  }
+
+  res.json({ products: productList });
+});
+
+app.get('/products/:pid', (req, res) => {
+  const productId = parseInt(req.params.pid, 10);
+
+  if (!isNaN(productId) && productId > 0) {
+    const product = productManagerInstance.getProductById(productId);
+
+    if (product) {
+      res.json({ product });
+    } else {
+      res.status(404).json({ error: `Producto con ID ${productId} no encontrado.` });
+    }
+  } else {
+    res.status(400).json({ error: 'El parámetro pid debe ser un número positivo.' });
+  }
+});
+
+app.post('/products', (req, res) => {
+  const { title, description, price, thumbnail, code, stock } = req.body;
+
+  try {
+    productManagerInstance.addProduct(title, description, price, thumbnail, code, stock);
+    res.json({ message: 'Producto agregado correctamente' });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Servidor Express iniciado en http://localhost:${port}`);
+});
